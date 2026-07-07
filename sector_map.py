3372a1dcfbd6e9b1ad9b8a715ccf251a2f25b508
dharnat222-dev <dharnat222@gmail.com>
2026-07-07 13:46:54 +0530

    Update sector_map.py

--- sector_map.py
+++ sector_map.py
@@ -1,2 +1,33 @@
-SECTOR_MAP = {
+SECTOR = {
+    "RELIANCE.NS": "Oil & Gas",
+    "ONGC.NS": "Oil & Gas",
+    "BPCL.NS": "Oil & Gas",
+    "IOC.NS": "Oil & Gas",
+
+    "HDFCBANK.NS": "Bank",
+    "ICICIBANK.NS": "Bank",
+    "SBIN.NS": "Bank",
+    "AXISBANK.NS": "Bank",
+    "KOTAKBANK.NS": "Bank",
+    "BANKBARODA.NS": "Bank",
+
+    "INFY.NS": "IT",
+    "TCS.NS": "IT",
+    "TECHM.NS": "IT",
+    "WIPRO.NS": "IT",
+    "HCLTECH.NS": "IT",
+
+    "TATAMOTORS.NS": "Auto",
+    "MARUTI.NS": "Auto",
+    "M&M.NS": "Auto",
+    "BAJAJ-AUTO.NS": "Auto",
+
+    "ULTRACEMCO.NS": "Cement",
+    "AMBUJACEM.NS": "Cement",
+    "ACC.NS": "Cement",
+
+    "SUNPHARMA.NS": "Pharma",
+    "CIPLA.NS": "Pharma",
+    "DRREDDY.NS": "Pharma",
+    "DIVISLAB.NS": "Pharma",
 }
