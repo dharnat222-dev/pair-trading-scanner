836cad73758b2b88e884bbf2728e5e7dc1d32e11
dharnat222-dev <dharnat222@gmail.com>
2026-07-07 13:12:10 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -30,3 +30,33 @@ print("Stocks Downloaded:", len(prices.columns))
 prices.to_csv("prices.csv")
 
 print("prices.csv created successfully")
+print("\nCalculating Correlation...")
+
+corr = prices.corr()
+
+pairs = []
+
+for i in range(len(corr.columns)):
+    for j in range(i + 1, len(corr.columns)):
+
+        s1 = corr.columns[i]
+        s2 = corr.columns[j]
+
+        c = corr.iloc[i, j]
+
+        if c >= 0.90:
+            pairs.append([s1, s2, round(c, 4)])
+
+pairs = sorted(pairs, key=lambda x: x[2], reverse=True)
+
+print("\nTop Correlated Pairs\n")
+
+for p in pairs[:50]:
+    print(p)
+
+pd.DataFrame(
+    pairs,
+    columns=["Stock1", "Stock2", "Correlation"]
+).to_csv("top_pairs.csv", index=False)
+
+print("\ntop_pairs.csv created")
